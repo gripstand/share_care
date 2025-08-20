b115a8b9f04cf982f8e427e0044bdc8bfb858825
gripstand <gripstand@gmail.com>
2025-08-20 09:13:02 -0400

    Implement feature X to enhance user experience and optimize performance

--- client/admin.py
+++ client/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Client, ReasonTopicsList
-
-# Register your models here.
+from .models import Client, ReasonTopicsList, CoreChallengeList, CommunicationsList, ReferralEntities, FundingSources # Register your models here.
 
 admin.site.register(Client)
 admin.site.register(ReasonTopicsList)
+admin.site.register(CoreChallengeList)
+admin.site.register(CommunicationsList)
+admin.site.register(ReferralEntities)
+admin.site.register(FundingSources)
